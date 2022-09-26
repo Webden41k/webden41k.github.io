9029b537e05dff03781e2862e87612a637c5c5dd
webden41k <webden41k@MacBook-Pro-webden41k.local>
2022-09-27 00:01:53 +0300

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$('.loader').click(function() {
+$('.loader').on( 'click' , function() {
   $(".loader-text").addClass("anim");
   $(".loader-text .name").addClass("animate__fadeOutLeft");
   $(".loader-text .spec").addClass("animate__fadeOutRight");

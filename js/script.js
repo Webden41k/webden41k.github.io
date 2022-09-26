5fe4efaf6948b675e03fd6d4c972d7382471f388
webden41k <webden41k@MacBook-Pro-webden41k.local>
2022-09-26 19:55:14 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+  $('body').circursor({
+
+    // size of cursor 1
+    size1: "0",
+
+    // size of cursor 2
+    size2: "30px",
+
+    // width of circle cursor
+    width: "10px",
+
+    // color of circle cursor
+    color: "white",
+
+    // duration of animation
+    duration: "200",
+
+    // easing effect
+    easing: "easeOutQuint"
+
+  });
+
+});

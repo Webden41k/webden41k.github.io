36cc4e488353497a0f42d7c87826683823b7c4be
webden41k <webden41k@MacBook-Pro-webden41k.local>
2022-12-15 17:14:26 +0200

    Initial commit

--- works/prizma/php/send.php
+++ works/prizma/php/send.php
@@ -1,18 +1,17 @@
 <?php 
-    $to = "webden41k@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['fname'];
-
-    $last_name = $_POST['lname'];
-
-    $subject = $_POST['type'];
-  
-    $message = $first_name . " " . $last_name . "\n\n" . $_POST['message'];
+    
+  $to = "youremail@mail.com"; // this is your Email address
+    
+  $from = $_POST['email'];
+  $first_name = $_POST['fname'];
+  $last_name = $_POST['lname'];
+  $subject = $_POST['type'];
+    
+  $message = $first_name . " " . $last_name . "\n\n" . $_POST['message'];
    
-
-    $headers = "From:" . $from;
+  $headers = "From:" . $from;
  
-    mail($to, $subject, $message, $headers);
-
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
+  mail($to, $subject, $message, $headers);
+  
+  // You can also use header('Location: index.html'); to redirect to another page.
 ?>
